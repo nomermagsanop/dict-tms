1dd91ff0fd19941ec9416d3620fd3e9209097dbc
nomermagsanop <144026469+nomermagsanop@users.noreply.github.com>
2024-04-17 09:36:30 +0800

    added user ui

---
+++ user/modal/office_add_modal.php
@@ -0,0 +1,30 @@
+<!-- Edit -->
+<div class="modal fade" id="addnew" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            	<div class="row float-left ml-2"><h4 class="modal-title float-left" id="myModalLabel">Add New Office</h4></div>
+                <div class="row float-right mr-2"><button type="button" class="close float-right" data-dismiss="modal" aria-hidden="true">&times;</button></div>
+            </div>
+            <div class="modal-body">
+			<div class="container-fluid">
+			<form method="POST">
+				<div class="row form-group">
+					<div class="col-sm-4">
+						<label class="control-label modal-label">Office Name</label>
+					</div>
+					<div class="col-sm-8">
+						<input type="text" class="form-control" name="office" value="" placeholder="Input office name.." required>
+					</div>
+				</div>
+            </div> 
+			</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                <button type="submit" name="edit" class="btn btn-success" id="addOffice"></i>Save</a>
+			</form>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ user/modal/office_edit_modal.php
@@ -0,0 +1,32 @@
+<!-- Edit -->
+<div class="modal fade" id="edit_<?php echo $host_id; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            	<div class="row float-left ml-2"><h4 class="modal-title float-left" id="myModalLabel">Edit Event</h4></div>
+                <div class="row float-right mr-2"><button type="button" class="close float-right" data-dismiss="modal" aria-hidden="true">&times;</button></div>
+            </div>
+            <div class="modal-body">
+			<div class="container-fluid">
+			<form method="POST">
+				<input type="hidden" class="form-control" name="host_id" value="<?php echo $host_id; ?>">
+				<div class="row form-group">
+					<div class="col-sm-4">
+						<label class="control-label modal-label">Office Name</label>
+					</div>
+					<div class="col-sm-8">
+						<input type="text" class="form-control" name="office" value="<?php echo $office; ?>">
+					</div>
+				</div>
+            </div> 
+			</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                <button type="submit" name="edit" class="btn btn-primary" id="updateOffice_<?php echo $host_id; ?>"></i>Update</a>
+			</form>
+            </div>
+
+        </div>
+    </div>
+</div>
+
